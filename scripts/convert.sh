d944066ddde38a77a456a5ef18c6f5ac634826a3
nishimoto <yuichiro.nishimoto@metagen.co.jp>
2022-01-01 16:01:38 +0900

    [修正] 汎用化

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# array_nb=(nb/00_はじめに.ipynb nb/01_Python入門.ipynb 02_Pandas入門.ipynb 03_Kaggleへ登録.ipynb 04_とりあえず機械学習してみる.ipynb 05_機械学習の精度を上げる（特徴量エンジニアリングのための準備）.ipynb 06_機械学習の精度を上げる（特徴量エンジニアリング）.ipynb 07_機械学習の精度を上げる（パラメーターチューニング）.ipynb 08_おわりに.ipynb 99_おまけ_Pandas応用.ipynb)
-# array_md=(nb/00_はじめに.md)
-
-# array_nb=(nb/01_Python入門.ipynb)
-# array_md=(nb/01_Python入門.md)
+path_out_folder="md"
 
 # ipynbからmdへ変換する用のスクリプト
 # "${array_nb[@]}"
@@ -21,15 +17,16 @@ for i in nb/*.md; do
 done
 
 # mdをフォルダ移動
-mv nb/*.md curriculum/
+mkdir -p $path_out_folder
+mv nb/*.md $path_out_folder
 
 # md内画像のコピー
 for folder in nb/*_files; do
-  mkdir -p curriculum/`basename $folder`/
-  cp -r $folder/* curriculum/`basename $folder`/
+  mkdir -p $path_out_folder/`basename $folder`/
+  cp -r $folder/* $path_out_folder/`basename $folder`/
   rm -r $folder
 done
 
 # nb内にあった画像の変換
-mkdir -p curriculum/assets/pic
-cp nb/assets/pic/* curriculum/assets/pic
+mkdir -p $path_out_folder/assets/pic
+cp nb/assets/pic/* $path_out_folder/assets/pic
